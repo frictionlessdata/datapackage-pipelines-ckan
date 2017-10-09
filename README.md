cb8b64ccdfb219de3b8d1826463b21df37777310
Brook Elgie <specialbrew@gmail.com>
2017-10-09 15:36:15 +0100

    Package setup

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # datapackage-pipelines-ckan
-Data Package Pipelines processors for CKAN
+
+[![Travis](https://img.shields.io/travis/frictionlessdata/datapackage-pipelines-ckan/master.svg)](https://travis-ci.org/frictionlessdata/datapackage-pipelines-ckan)
+[![Coveralls](http://img.shields.io/coveralls/frictionlessdata/datapackage-pipelines-ckan/master.svg)](https://coveralls.io/r/frictionlessdata/datapackage-pipelines-ckan?branch=master)
+[![PyPi](https://img.shields.io/pypi/v/datapackage-pipelines-ckan.svg)](https://pypi.python.org/pypi/datapackage-pipelines-ckan)
+[![SemVer](https://img.shields.io/badge/versions-SemVer-brightgreen.svg)](http://semver.org/)
+[![Gitter](https://img.shields.io/gitter/room/frictionlessdata/chat.svg)](https://gitter.im/frictionlessdata/chat)
+
+[Data Package Pipelines](https://github.com/frictionlessdata/datapackage-pipelines) processors for [CKAN](https://ckan.org/).
+
+
+## Install
+
+```
+# clone the repo and install it with pip
+
+git clone https://github.com/frictionlessdata/datapackage-pipelines-ckan.git
+pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import os
+import io
+from setuptools import setup, find_packages
+
+
+# Helpers
+def read(*paths):
+    """Read a text file."""
+    basedir = os.path.dirname(__file__)
+    fullpath = os.path.join(basedir, *paths)
+    contents = io.open(fullpath, encoding='utf-8').read().strip()
+    return contents
+
+
+# Prepare
+PACKAGE = 'datapackage_pipelines_ckan'
+NAME = PACKAGE.replace('_', '-')
+INSTALL_REQUIRES = [
+    'datapackage-pipelines>=1.0,<2.0'
+]
+TESTS_REQUIRE = [
+    'pylama',
+    'tox',
+    'mock'
+]
+README = read('README.md')
+VERSION = read(PACKAGE, 'VERSION')
+PACKAGES = find_packages(exclude=['examples', 'tests'])
+
+
+# Run
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=PACKAGES,
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require={'develop': TESTS_REQUIRE},
+    zip_safe=False,
+    long_description=README,
+    description='{{ DESCRIPTION }}',
+    author='Open Knowledge International',
+    author_email='info@okfn.org',
+    url='https://github.com/frictionlessdata/datapackage-pipelines-ckan',
+    license='MIT',
+    keywords=[
+        'data', 'ckan'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
